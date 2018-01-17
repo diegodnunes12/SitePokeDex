f67cab9cc9d0c1f21ac1e65743e3d368f204b53c
Diego Nunes <diegodnunes12@gmail.com>
2018-01-16 22:15:03 -0200

    Criando repeter dos pokemons

--- SitePokeDex/Default.aspx
+++ SitePokeDex/Default.aspx
@@ -2,5 +2,28 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-        teste
+        
+    <section>
+        <article>
+            <asp:Repeater ID="RptListPokemon" runat="server">
+                <ItemTemplate>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="col-md-4">
+
+                                </div>
+                                <div class="col-md-8">
+                                    <h2>
+                                        <%# Eval("Nome") %>
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </article>
+    </section>
+
 </asp:Content>
--- SitePokeDex/Default.aspx.cs
+++ SitePokeDex/Default.aspx.cs
@@ -11,7 +11,39 @@ namespace SitePokeDex
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                List<Pokemon> pokemons = new List<Pokemon>();
 
+                Pokemon pokemon1 = new Pokemon();
+                pokemon1.Id = "1";
+                pokemon1.Nome = "Poke1";                
+
+                Pokemon pokemon2 = new Pokemon();
+                pokemon2.Id = "2";
+                pokemon2.Nome = "Poke2";
+
+                Pokemon pokemon3 = new Pokemon();
+                pokemon3.Id = "3";
+                pokemon3.Nome = "Poke3";
+
+                Pokemon pokemon4 = new Pokemon();
+                pokemon4.Id = "4";
+                pokemon4.Nome = "Poke4";
+
+                Pokemon pokemon5 = new Pokemon();
+                pokemon5.Id = "5";
+                pokemon5.Nome = "Poke5";
+
+                pokemons.Add(pokemon1);
+                pokemons.Add(pokemon2);
+                pokemons.Add(pokemon3);
+                pokemons.Add(pokemon4);
+                pokemons.Add(pokemon5);
+
+                this.RptListPokemon.DataSource = pokemons;
+                this.RptListPokemon.DataBind();
+            }
         }
     }
 }
